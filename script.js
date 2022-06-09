f4a5a4c19873e711969ae380e5344940d9cf684c
jaycek <62276154+jaycek@users.noreply.github.com>
2022-06-09 17:05:59 +0530

    Init

---
+++ script.js
@@ -0,0 +1,9 @@
+var firstname = "Jayce";
+let lname="Alex";
+let age=22;
+
+document.write(firstname);
+document.write("<br>");
+document.write(lname);
+document.write("<br>");
+document.write(age);
